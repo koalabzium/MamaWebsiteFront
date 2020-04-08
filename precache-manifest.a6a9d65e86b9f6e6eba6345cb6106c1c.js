723237637e36760ad45c7f2a10717235fe2aecdd
koalabzium <zosia.owsiak@gmail.com>
2020-04-08 13:45:01 +0200

    Updates

---
+++ precache-manifest.a6a9d65e86b9f6e6eba6345cb6106c1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1190ec8a0a6b72301299858d9bf356e",
+    "url": "/MamaWebsiteFront/index.html"
+  },
+  {
+    "revision": "bdc25e61d1348b18b1da",
+    "url": "/MamaWebsiteFront/static/css/2.2990c7a7.chunk.css"
+  },
+  {
+    "revision": "17f9cd292d5617a0dd1a",
+    "url": "/MamaWebsiteFront/static/css/main.df705a8f.chunk.css"
+  },
+  {
+    "revision": "bdc25e61d1348b18b1da",
+    "url": "/MamaWebsiteFront/static/js/2.50f09b6a.chunk.js"
+  },
+  {
+    "revision": "dc2131dd5acc35a82da51cf4f3979c7d",
+    "url": "/MamaWebsiteFront/static/js/2.50f09b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f9cd292d5617a0dd1a",
+    "url": "/MamaWebsiteFront/static/js/main.a67d849b.chunk.js"
+  },
+  {
+    "revision": "3ed50cb1af463090fdbf",
+    "url": "/MamaWebsiteFront/static/js/runtime-main.362a35ca.js"
+  }
+]);
