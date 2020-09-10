928eb72acc95333f1ed5d47d02ab22deec302de4
koalabzium <zosia.owsiak@gmail.com>
2020-09-10 09:52:49 +0200

    Sorted categories

--- src/components/manageCategories.jsx
+++ src/components/manageCategories.jsx
@@ -21,6 +21,9 @@ class ManageCategories extends Component {
       ...cat,
       edited: false,
     }));
+    categories_data.sort(function (a, b) {
+      return a.name > b.name ? 1 : b.name > a.name ? -1 : 0;
+    });
     this.setState({ categories: categories_data });
   }
 
