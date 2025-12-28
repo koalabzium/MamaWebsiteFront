a7d8fdb5aa59c5a9b5c3464354b967f8a0f572ea
Warzecha <damian.warzecha@viviclabs.com>
2025-12-28 22:51:22 +0100

    Fix mobile layout

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const PAGES_RANGE = 3;
+
 const Pagination = (props) => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize);
-  if (pagesCount <= 1) return null;
-  const pages = [...Array(pagesCount).keys()].map((i) => i + 1);
+  if (pagesCount <= 1) {
+    return null
+  };
+
+  const pagesStart = Math.max(0, currentPage - PAGES_RANGE);
+  const pagesEnd = Math.min(currentPage + PAGES_RANGE - 1, pagesCount);
+  const pagesToShow = pagesEnd - pagesStart;
+  const pages = [...Array(pagesToShow).keys()].map((i) => pagesStart + i + 1);
+
+  console.log(pages);
   return (
     <React.Fragment>
       <nav aria-label="Page navigation example">
         <ul className="pagination">
 
-        <li
-              key={'prev'}
-              className={
-                currentPage > 1 ? "page-item" : "page-item disabled"
-              }
-            >
-              <a className="page-link" onClick={() => onPageChange(currentPage-1)}>
-                Poprzednia
-              </a>
-            </li>
+          <li
+            key={'prev'}
+            className={
+              currentPage > 1 ? "page-item" : "page-item disabled"
+            }
+          >
+            <a className="page-link" onClick={() => onPageChange(currentPage - 1)}>
+              Poprzednia
+            </a>
+          </li>
 
 
           {pages.map((page) => (
@@ -37,16 +47,16 @@ const Pagination = (props) => {
           ))}
 
 
-            <li
-              key={'next'}
-              className={
-                currentPage < pagesCount ? "page-item" : "page-item disabled"
-              }
-            >
-              <a className="page-link" onClick={() => onPageChange(currentPage+1)}>
-                Następna
-              </a>
-            </li>
+          <li
+            key={'next'}
+            className={
+              currentPage < pagesCount ? "page-item" : "page-item disabled"
+            }
+          >
+            <a className="page-link" onClick={() => onPageChange(currentPage + 1)}>
+              Następna
+            </a>
+          </li>
 
 
         </ul>
