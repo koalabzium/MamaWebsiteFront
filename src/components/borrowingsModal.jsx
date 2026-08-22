f8328682c4fe3e228b1ad38c5cd2e21b8b929586
Warzecha <damian.warzecha@viviclabs.com>
2026-08-22 18:13:27 +0200

    Convert manageReaders/borrowingsModal to function components using useManagedList

--- src/components/borrowingsModal.jsx
+++ src/components/borrowingsModal.jsx
@@ -1,44 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import Modal from "react-bootstrap/Modal";
 import "react-confirm-alert/src/react-confirm-alert.css";
 
-class BorrowingsModal extends Component {
-  render() {
-    const { borrowings, onCancelBorrowing } = this.props;
-
-    return (
-      <Modal
-        onHide={this.props.onHide}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={borrowings != null}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Wypożyczenia
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {(borrowings || [])
-            .filter((b) => b.active)
-            .map((b) => (
-              <div>
-                <span>
-                  {b.bookTitle} - {b.date}
-                </span>
-                <button
-                  className="btn btn-outline-danger btn-sm"
-                  onClick={() => onCancelBorrowing(b)}
-                >
-                  X
-                </button>
-              </div>
-            ))}
-        </Modal.Body>
-      </Modal>
-    );
-  }
-}
+const BorrowingsModal = ({ borrowings, onCancelBorrowing, onHide }) => (
+  <Modal
+    onHide={onHide}
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    show={borrowings != null}
+  >
+    <Modal.Header closeButton>
+      <Modal.Title id="contained-modal-title-vcenter">
+        Wypożyczenia
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      {(borrowings || [])
+        .filter((b) => b.active)
+        .map((b) => (
+          <div key={b.id}>
+            <span>
+              {b.bookTitle} - {b.date}
+            </span>
+            <button
+              className="btn btn-outline-danger btn-sm"
+              onClick={() => onCancelBorrowing(b)}
+            >
+              X
+            </button>
+          </div>
+        ))}
+    </Modal.Body>
+  </Modal>
+);
 
 export default BorrowingsModal;
